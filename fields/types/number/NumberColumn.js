93f6dbd5a96832ea7a590b1e26979af70410e1b7
Simon Facciol <simon.facciol@gmail.com>
2018-03-23 20:52:42 +0100

    Attempt to fix money value display

--- fields/types/number/NumberColumn.js
+++ fields/types/number/NumberColumn.js
@@ -4,29 +4,49 @@ import ItemsTableCell from '../../components/ItemsTableCell';
 import ItemsTableValue from '../../components/ItemsTableValue';
 
 var NumberColumn = React.createClass({
-	displayName: 'NumberColumn',
-	propTypes: {
-		col: React.PropTypes.object,
-		data: React.PropTypes.object,
-	},
-	renderValue () {
-		const value = this.props.data.fields[this.props.col.path];
-		if (!value || isNaN(value)) return null;
+  displayName: 'NumberColumn',
+  propTypes  : {
+    col : React.PropTypes.object,
+    data: React.PropTypes.object,
+    list: React.PropTypes.object
+  },
+  renderValue () {
+    const value = this.props.data.fields[this.props.col.path];
+    if (!value || isNaN(value)) return null;
 
-		const format = this.props.col.format || '$0,0.00';
-		const formattedValue = (this.props.col.type === 'money') ? numeral(value).format(format) : value;
+    let formattedValue = value;
 
-		return formattedValue;
-	},
-	render () {
-		return (
-			<ItemsTableCell>
-				<ItemsTableValue field={this.props.col.type}>
-					{this.renderValue()}
-				</ItemsTableValue>
-			</ItemsTableCell>
-		);
-	},
+    if (this.props.col.type === 'money') {
+      const currency = this.props.list.fields[this.props.col.path].currency || 'en-gb';
+      const format = this.props.list.fields[this.props.col.path].format || '$0,0.00';
+
+      if (currency) {
+        try {
+          numeral.language(currency, require('numeral/languages/' + currency));
+          numeral.language(currency);
+        } catch (err) {
+          throw new Error('NumberColumn: currency failed to load.');
+        }
+      }
+
+      if (format) {
+        formattedValue = numeral(value).format(format);
+      } else {
+        formattedValue = numeral(value);
+      }
+    }
+
+    return formattedValue;
+  },
+  render () {
+    return (
+        <ItemsTableCell>
+          <ItemsTableValue field={this.props.col.type}>
+            {this.renderValue()}
+          </ItemsTableValue>
+        </ItemsTableCell>
+    );
+  },
 });
 
 module.exports = NumberColumn;
