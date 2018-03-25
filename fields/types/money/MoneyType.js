22cfc33708011f02d846e581901be11eb3903914
Simon Facciol <simon.facciol@gmail.com>
2018-03-25 03:57:54 +0200

    Fix money formatting in columns

--- fields/types/money/MoneyType.js
+++ fields/types/money/MoneyType.js
@@ -8,17 +8,18 @@ var util = require('util');
  * @extends Field
  * @api public
  */
-function money (list, path, options) {
-	this.currency = options.currency;
-	this._nativeType = Number;
-	this._underscoreMethods = ['format'];
-	this._properties = ['currency'];
-	this._fixedSize = 'small';
-	this._formatString = (options.format === false) ? false : (options.format || '$0,0.00');
-	if (this._formatString && typeof this._formatString !== 'string') {
-		throw new Error('FieldType.Money: options.format must be a string.');
-	}
-	money.super_.call(this, list, path, options);
+function money(list, path, options) {
+  this.currency = options.currency;
+  this._nativeType = Number;
+  this._underscoreMethods = ['format'];
+  this._properties = ['currency', 'formatString'];
+  this._fixedSize = 'small';
+  this.formatString = (options.format === false) ? false : (options.format || '$0,0.00');
+  this._formatString = (options.format === false) ? false : (options.format || '$0,0.00');
+  if (this._formatString && typeof this._formatString !== 'string') {
+    throw new Error('FieldType.Money: options.format must be a string.');
+  }
+  money.super_.call(this, list, path, options);
 }
 money.properName = 'Money';
 util.inherits(money, FieldType);
@@ -36,19 +37,19 @@ money.prototype.addFilterToQuery = NumberType.prototype.addFilterToQuery;
  * Formats the field value
  */
 money.prototype.format = function (item, format) {
-	if (this.currency) {
-		try {
-			numeral.language(this.currency, require('numeral/languages/' + this.currency));
-			numeral.language(this.currency);
-		} catch (err) {
-			throw new Error('FieldType.Money: options.currency failed to load.');
-		}
-	}
-	if (format || this._formatString) {
-		return (typeof item.get(this.path) === 'number') ? numeral(item.get(this.path)).format(format || this._formatString) : '';
-	} else {
-		return item.get(this.path) || '';
-	}
+  if (this.currency) {
+    try {
+      numeral.language(this.currency, require('numeral/languages/' + this.currency));
+      numeral.language(this.currency);
+    } catch (err) {
+      throw new Error('FieldType.Money: options.currency failed to load.');
+    }
+  }
+  if (format || this._formatString) {
+    return (typeof item.get(this.path) === 'number') ? numeral(item.get(this.path)).format(format || this._formatString) : '';
+  } else {
+    return item.get(this.path) || '';
+  }
 };
 
 /* Export Field Type */
--- fields/types/number/NumberColumn.js
+++ fields/types/number/NumberColumn.js
@@ -1,7 +1,33 @@
 import React from 'react';
-import numeral from 'numeral';
 import ItemsTableCell from '../../components/ItemsTableCell';
 import ItemsTableValue from '../../components/ItemsTableValue';
+import numeral from 'numeral';
+
+numeral.register('locale', 'eur', {
+  delimiters   : {
+    thousands: ',',
+    decimal  : '.'
+  },
+  abbreviations: {
+    thousand: 'k',
+    million : 'm',
+    billion : 'b',
+    trillion: 't'
+  },
+  ordinal      : function (number) {
+    var b = number % 10;
+    return (~~(number % 100 / 10) === 1) ? 'th' :
+        (b === 1) ? 'st' :
+            (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+  },
+  currency     : {
+    symbol: '€'
+  }
+});
+
+// switch between locales
+numeral.locale('eur');
 
 var NumberColumn = React.createClass({
   displayName: 'NumberColumn',
@@ -12,17 +38,14 @@ var NumberColumn = React.createClass({
   },
   renderValue () {
     const value = this.props.data.fields[this.props.col.path];
-    if (!value || isNaN(value)) return null;
+    if ((!value || isNaN(value)) && value != 0) return null;
 
     let formattedValue = value;
 
     if (this.props.col.type === 'money') {
-      const format = this.props.list.fields[this.props.col.path].format || false;
+      const format = this.props.list.fields[this.props.col.path].formatString || '$ 0,0.00';
 
-      if (format) {
-        formattedValue = numeral(value).format(format);
-      }
-
+      formattedValue = numeral(value).format(format);
     }
 
     return formattedValue;
